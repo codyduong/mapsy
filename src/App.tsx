5b623435cf1607b4dc809349537f37c022ac1f5a
unknown <thapaanil2007@gmail.com>
2022-04-09 14:40:12 -0500

    Add weather api

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import GoogleMap from './components/map';
 import { ThemeProvider } from './contexts/Theme';
 import styled from 'styled-components';
 import { SearchBar } from './components/searchbar';
+import api from './api';
 
 const AppWrapper = styled.div`
     width: 100vw;
@@ -9,6 +10,11 @@ const AppWrapper = styled.div`
 `;
 
 function App() {
+    (async () => {
+        console.log(await api.getWeather({        lat: 38.971488,
+            lng: -95.325759}));
+    })();
+
     return (
         <ThemeProvider>
             <AppWrapper>
