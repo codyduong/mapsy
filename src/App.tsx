afc0f47e1e9550efc8141224147b2aef8614d624
Alexis <alexis.vielma.us@gmail.com>
2022-04-09 00:29:04 -0500

    Remove unnecessary files and packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement, useEffect, useRef } from 'react';
 import './App.css';
 
+import { Status, Wrapper } from '@googlemaps/react-wrapper';
+
+function MyMapComponent({ center, zoom }: { center: google.maps.LatLngLiteral; zoom: number }) {
+	const ref = useRef();
+
+	useEffect(() => {
+		// @ts-ignore
+		new window.google.maps.Map(ref.current!, {
+			center,
+			zoom,
+		});
+	});
+
+	// @ts-ignore
+	return <div ref={ref!} id="map" />;
+}
+
+const render = (status: Status): ReactElement => {
+	if (status === Status.LOADING) return <h3>{status} ..</h3>;
+	if (status === Status.FAILURE) return <h3>{status} ...</h3>;
+	// @ts-ignore
+	return null;
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const center = { lat: -34.397, lng: 150.644 };
+	const zoom = 4;
+
+	return (
+		<Wrapper apiKey={'AIzaSyCdrceqUk17BLwecJBBRzLqJXDnvL_NSAE'} render={render}>
+			<MyMapComponent center={center} zoom={zoom} />
+		</Wrapper>
+	);
 }
 
 export default App;
